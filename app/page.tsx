cfd51c6087f35ef23d2920d6b7ca0a50ae4e592d
An Ngo <duy.an18041999@gmail.com>
2023-08-13 22:20:17 +0700

    Add time tracker + todo list + clock

--- app/page.tsx
+++ app/page.tsx
@@ -1,112 +1,200 @@
+'use client';
+import { randomUUID } from 'crypto';
 import Image from 'next/image'
+import { useEffect, useState, useRef } from 'react';
+const uuid = require('uuid');
+
+type Activity = {
+  name: string;
+  id: string;
+  timeTrackedInSec: number;
+  subActivities: Activity[];
+  isDone: boolean;
+}
 
 export default function Home() {
-  return (
+  const [startDate, setStartDate]                 = useState(new Date())
+  const [workTimeInSec, setWorkTimeInSec]         = useState(0);
+  const [isTracking, setIsTracking] = useState(false);
+  const currentActivity = useRef<Activity>();
+
+  const [isActive, setIsActive]                   = useState(false);
+  const [lastActiveDate, setLastActiveDate]       = useState(new Date());
+  const [inactiveTimeInSec, setInActiveTimeInSec] = useState(0);
+  
+  const [currentTime, setCurrentTime]             = useState(new Date());
+  const [activityList, setActivityList]           = useState<Activity[]>();
+  const [newActivityName, setNewActivityName]     = useState('');
+  const [isClient, setIsClient] = useState(false)
+
+  const startTracking = (activity: Activity) => {
+    currentActivity.current = activity;
+    setIsTracking(true);
+  }
+
+  const stopTracking = () => {
+    currentActivity.current = undefined;
+    setIsTracking(false);
+  }
+
+  const updateTimer  = () => {
+    let date = new Date();
+    // set tracking time in the case we are tracking
+    if(currentActivity.current !== undefined){
+      let diff = date.getTime() - startDate.getTime(); 
+      let newWorkTimeInSec = Math.floor(diff / 1000)
+      setWorkTimeInSec(newWorkTimeInSec);
+      console.log(newWorkTimeInSec - workTimeInSec);
+      currentActivity.current.timeTrackedInSec += (newWorkTimeInSec - workTimeInSec);
+
+      let activeDiff = date.getTime() - lastActiveDate.getTime(); 
+      let inActiveInSec = Math.floor(activeDiff / 1000)
+      setInActiveTimeInSec(inactiveTimeInSec)
+      if(inActiveInSec > 5){
+        setIsActive(false);
+      }
+
+      if(inActiveInSec > 10){
+        currentActivity.current = undefined;
+      }
+    }
+  }
+
+  const onActive = () => {
+    setIsActive(true)
+    setLastActiveDate(new Date())
+  }
+
+  const createActivityForm = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if(newActivityName !== ''){
+      createActivity(newActivityName);
+      setNewActivityName('');
+    }
+  }
+
+  const createActivity = (activityName: string) => {
+    let newActivity: Activity = {
+      name: activityName,
+      id: uuid.v4(),
+      timeTrackedInSec: 0,
+      subActivities: [],
+      isDone: false
+    };
+    if(activityList){
+      setActivityList([...activityList, newActivity]);
+      return;
+    }
+    setActivityList([newActivity]);
+  }
+
+  const completeActivity = (activity: Activity) => {
+    activity.isDone = true;
+  }
+
+  const updateStorage = () => {
+    console.log('update local storage')
+    localStorage.setItem('activities', JSON.stringify(activityList));
+  }
+
+  if (typeof window !== "undefined") {
+    window.onbeforeunload = () => {
+      updateStorage();
+    }
+  }
+
+  useEffect(() => {
+    setIsClient(true);
+    window.addEventListener('mousemove', onActive);
+    window.addEventListener('keyup', onActive);
+    window.addEventListener('onscroll', onActive);
+    const clockInterval = setInterval(() => {setCurrentTime(new Date())}, 1000);
+    if(localStorage.getItem('activities') !== 'undefined'){
+      console.log(localStorage.getItem('activities'))
+      const activityListInStorage = JSON.parse(localStorage.getItem('activities') || '[]')
+      if(activityListInStorage){
+        setActivityList(activityListInStorage);
+      }
+    }
+    return () => {
+      clearInterval(clockInterval);
+    };
+  }, [])
+
+  useEffect(() => {
+    const updateTime = setTimeout(updateTimer, 1000);
+    return () => {
+      clearTimeout(updateTime);
+    };
+  })
+
+  useEffect(() => {
+    if(currentActivity){
+      setStartDate(new Date());
+    }
+  }, [currentActivity])
+
+  const renderClock = () => {
+    let hour = currentTime.getHours();
+    let minute = currentTime.getMinutes();
+    let seconds = currentTime.getSeconds();
+    let timeSuffix = 'AM'
+    if(hour > 12){
+      hour = hour - 12;
+      timeSuffix = 'PM';
+    }
+    return (
+      <div className='clock-container flex'>
+        <div>{hour}</div>:
+        <div>{minute}</div>:
+        <div>{seconds}</div> 
+        <div>{' ' + timeSuffix}</div>
+      </div>
+    )
+  }
+
+  const renderActivity = (activity: Activity) => {
+    let isCurrentActivity = currentActivity.current && currentActivity.current.id === activity.id;
+    return (<div className='activity-container flex' key={activity.id}>
+      <div className='activity-name flex basis-1/2'>{activity.name}</div>
+      <div className='activity-tacktime flex basis-1/4'>Tracked time: {activity.timeTrackedInSec}</div>
+      <div className='activity-actions flex basis-1/4'> 
+        {!isCurrentActivity && <button className='activity-action-btn' onClick={() => startTracking(activity)}>Start</button>}
+        {isCurrentActivity && <button className='activity-action-btn' onClick={() => stopTracking}>Stop</button>}
+        <button className='activity-action-btn' onClick={() => completeActivity(activity)}>Mark as done</button>
+      </div>
+    </div>);
+  }
+
+  return (isClient &&
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+      <div className='container'>
+        {renderClock()}
+        <div className='activity-label'>Activity list</div>
+        <form onSubmit={(e) => createActivityForm(e)} className='create-activity-form flex'>
+          <input 
+            className='activity-create-name-input flex basis-3/4'
+            placeholder='Enter the task you want to do' 
+            value={newActivityName} 
+            onChange={(e) => setNewActivityName(e.target.value)}>
+          </input>
+          <button type='submit' className='activity-create-btn flex basis-1/4'>Create new activity</button>
+        </form>
+        {activityList && activityList.map((element: Activity, index) => {
+            return renderActivity(element);
+          })
+        }
+        <div className='timer'>
+          Current time: {workTimeInSec}
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+        <div className='activeTimer'>
+          Inactive time: {inactiveTimeInSec}
+        </div>
+        <div>Last active date {lastActiveDate.toISOString()}</div>
+        {!isActive && <div>No activity found</div>}
+        <div className='action-container'>
+          
+        </div>
       </div>
     </main>
   )
